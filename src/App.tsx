921eff5163209bfbea78ae664918a2af1a57eaa4
Jose Mollan <jmollan@gmail.com>
2023-06-30 11:26:00 -0500

    segunda subida

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Subida a Produccion</h1>
+      <h1>Libertad!!! Saliendo a buscar otro cerrito</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
